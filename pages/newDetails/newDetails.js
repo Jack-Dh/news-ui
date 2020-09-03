02d22c93021bb8a9aea8ea3d6643a4c205483a4d
jack <jack.zhou@tctchina.com.cn>
2020-09-03 17:39:13 +0800

    添加日期，添加公共样式

--- pages/newDetails/newDetails.js
+++ pages/newDetails/newDetails.js
@@ -2,84 +2,89 @@
 const app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    id: "",
-    data: ""
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        id: "",
+        data: ""
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    console.log(options)
-    this.setData({
-      id: options.id
-    })
-    this.getNewsDetailsData()
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        console.log(options)
+        this.setData({
+            id: options.id
+        })
+        this.getNewsDetailsData()
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
 
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
 
-  },
-  /**
-   * 获取新闻详情
-   */
-  getNewsDetailsData: function () {
-    app.http.http(app.apis.gethotNesContent, "get", {
-      id: this.data.id
-    }).then(res => {
-      this.setData({
-        data: res.data
-      })
-      console.log(res)
-    })
-  }
+    },
+    /**
+     * 获取新闻详情
+     */
+    getNewsDetailsData: function() {
+        app.http.http(app.apis.gethotNesContent, "get", {
+            id: this.data.id
+        }).then(res => {
+            res.data.imgUrls = res.data.imgUrls.map(item => {
+                if (item.includes('undefined')) {
+                    return item.substring(9)
+                }
+                return item
+            })
+            this.setData({
+                data: res.data
+            })
+        })
+    }
 })
