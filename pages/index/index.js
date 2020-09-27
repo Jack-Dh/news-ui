05cb0597e848d6330907da3f258311266effa894
jack <jack.zhou@tctchina.com.cn>
2020-09-27 13:23:02 +0800

    下拉加载

--- pages/index/index.js
+++ pages/index/index.js
@@ -76,15 +76,13 @@ Page({
             wx.hideNavigationBarLoading() //完成停止加载
             wx.stopPullDownRefresh() //停止下拉刷新
             this.setData({
-                data: res.data
+                data: res.data,
             })
-            console.log(res)
         })
     },
 
     //下拉刷新
     onPullDownRefresh: function() {
-        console.log(1)
         wx.showNavigationBarLoading() //在标题栏中显示加载
         this.getNewsTitle()
             // wx.showNavigationBarLoading() //在标题栏中显示加载
